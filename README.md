36a71c1e927b529f97b046d287b9396d7394ff1b
jazminejose <jazzi.lyn217@gmail.com>
2021-12-11 00:35:52 -0800

    run app

---
+++ README.md
@@ -0,0 +1,41 @@
+# README Generator
+  ![Badge for GitHub commit activity](https://img.shields.io/github/commit-activity/w/jazminejose/teamProfile?style=for-the-badge)
+
+## Table of Content(s)
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+The Team Profile project generates a Software Engineering Team using node CLI. The user is prompted with questions regarding information about the team they would like to build.
+
+Walkthrough Video: [Screencastify](https://watch.screencastify.com/v/stb1hmr0nqKDUyNIDaFO)
+
+<img src="https://user-images.githubusercontent.com/92974218/145670167-29952e89-075b-465d-a3bd-d5aa299fbc8b.png">
+
+## Installation
+ [jest](https://jestjs.io/)
+ [inquirer](https://www.npmjs.com/package/inquirer)
+
+## Usage
+Run the following command: 
+`node app.js`
+
+## Contributing
+None
+
+## Tests
+No available tests
+
+## License
+MIT
+
+## Questions
+For any questions, please contact me:<br>
+Email - jazzi.lyn217@yahoo.com<br>
+Github - https://github.com/jazminejose<br>
--- app.js
+++ app.js
@@ -17,11 +17,11 @@ let team = [];
 const addEmployee = () => {
   inquirer.prompt({
     type: "confirm",
-    name: "addEmployee",
+    name: "add",
     message: "Would you like to add another employee?"
   })
     .then(data => {
-      if (data.addEmployee == true) {
+      if (data.add == true) {
         questionPrompts()
       }
       else {
@@ -34,28 +34,28 @@ const addEmployee = () => {
       }
     })
 }
-
+// array of questions for all employees
 function questionPrompts() {
   inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "Please enter name of employee."
+      message: "Please enter name of employee:"
     },
     {
       type: "input",
       name: "id",
-      message: "Please enter ID of employee."
+      message: "Please enter ID of employee:"
     },
     {
       type: "input",
       name: "email",
-      message: "Please enter employee email."
+      message: "Please enter employee email:"
     },
     {
       type: "list",
       name: "role",
-      message: "Select current role.",
+      message: "Select current role:",
       choices: [
         "Manager",
         "Engineer",
@@ -68,10 +68,11 @@ function questionPrompts() {
         inquirer.prompt([{
           type: "input",
           name: "officeNumber",
-          message: "Please enter office number."
+          message: "Please enter office number:"
         }])
           .then(addManager => {
             const newManager = new Manager(answers.name, answers.id, answers.email, addManager.officeNumber)
+            console.log(newManager)
             team.push(newManager)
             addEmployee()
           })
@@ -79,10 +80,11 @@ function questionPrompts() {
         inquirer.prompt([{
           type: "input",
           name: "github",
-          message: "Please enter gitHub of employee."
+          message: "Please enter gitHub of employee:"
         }])
           .then(addEngineer => {
             const newEngineer = new Engineer(answers.name, answers.id, answers.email, addEngineer.github)
+            console.log(newEngineer)
             team.push(newEngineer)
             addEmployee()
           })
@@ -90,10 +92,11 @@ function questionPrompts() {
         inquirer.prompt([{
           type: "input",
           name: "school",
-          message: "Please enter school of intern."
+          message: "Please enter school of intern:"
         }])
           .then(addIntern => {
             const newIntern = new Intern(answers.name, answers.id, answers.email, addIntern.school)
+            console.log(newIntern)
             team.push(newIntern)
             addEmployee()
           })
@@ -106,33 +109,9 @@ function writeToFile(fileName, data) {
     if (err) {
       console.log(err)
     }
-    console.log("Employee summary has been created.")
+    console.log(team)
+    console.log("Success! Employee summary has been created.")
   })
 }
 
 questionPrompts()
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
